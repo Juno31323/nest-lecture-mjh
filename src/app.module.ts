0d0d312bff9892d54e7a1aa2f982b1b0a99b3939
Juno31323 <juno31323@gmail.com>
2025-03-06 15:22:44 +0900

    1111

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,12 @@ import { BldgModule } from './bldg/bldg.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.CLOUDSQL_HOST,
-      port: parseInt(process.env.CLOUDSQL_PORT || '5432', 10), // 기본값 설정1
+      port: parseInt(process.env.CLOUDSQL_PORT || '5432', 10), // 기본값 설정1212
       username: process.env.CLOUDSQL_USER,
       password: process.env.CLOUDSQL_PASS,
       database: process.env.CLOUDSQL_DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      extra: { 
-        // Cloud SQL Unix socket 경로 설정 
-         socketPath: `/cloudsql/${process.env.CLOUDSQL_CONNECTION_NAME}`, 
-      },
-      
+      synchronize: false,      
     }),
     BldgModule,
     ],
