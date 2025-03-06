950ca7d9312c978d7b7593c9272ae46364c16bd6
Juno31323 <juno31323@gmail.com>
2025-03-06 14:32:28 +0900

    testplz

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  // 포트는 process.env.PORT에서 가져오고, 기본값은 8080
+  const port = process.env.PORT || 8080;
+  
+  // 0.0.0.0에 바인딩하여 외부 요청 수신 가능하도록 설정
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
